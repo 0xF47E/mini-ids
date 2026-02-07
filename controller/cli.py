3454b51c5c66cd6445f046c7e68eaae70c51097e
0xF47E <0xF47E@proton.me>
2026-02-07 21:34:54 +0100

    Added a comment to the cli entrypoint

--- controller/cli.py
+++ controller/cli.py
@@ -2,6 +2,8 @@ from ids.runner import scan
 from ids.parser import parse
 from ids.datastorage import save
 
+# This is the entrypoint of the controller
+
 def main():
     result = scan("127.0.0.1", "22")
     records = parse(result)
