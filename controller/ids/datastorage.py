2e832a6db893b258df28e08f364d4e2295820720
0xF47E <0xF47E@proton.me>
2026-02-08 12:48:17 +0100

    docker: added Dockerfile

--- controller/ids/datastorage.py
+++ controller/ids/datastorage.py
@@ -1,9 +1,12 @@
 import json
+from pathlib import Path
 
 # This has to be a database later
+PROJECT_PATH = Path(__file__).resolve().parent.parent.parent
+DATA_PATH = PROJECT_PATH / "data"
 
 def save(outfile, records):
-    with open(outfile, "a") as f:
+    with open(str(DATA_PATH / "scan.log"), "a") as f:
         for record in records:
             f.write(json.dumps(record) + "\n")
         f.close()
