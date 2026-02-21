391c3a8c823a8bbb6b093a520d2356731cf54bcc
0xF47E <0xF47E@proton.me>
2026-02-21 16:36:49 +0100

    controller: simple database integration with sqlite3, only saving for now

--- controller/ids/datastorage.py
+++ controller/ids/datastorage.py
@@ -1,12 +1,60 @@
 import json
 from pathlib import Path
+import sqlite3
 
 # This has to be a database later
 PROJECT_PATH = Path(__file__).resolve().parent.parent.parent
 DATA_PATH = PROJECT_PATH / "data"
 
-def save(records):
-    with open(str(DATA_PATH / "scan.log"), "a") as f:
-        for record in records:
-            f.write(json.dumps(record) + "\n")
-        f.close()
+def get_connection():
+    con = sqlite3.connect("data/ids.db")
+    con.execute("PRAGMA foreign_keys = ON;")
+    return con
+
+def init_db():
+    con = get_connection()
+    cur = con.cursor()
+    cur.execute("""
+        CREATE TABLE IF NOT EXISTS scans(
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            started_at TIMESTAMP,
+            scope TEXT
+        );""")
+    con.commit()
+    cur.execute("""
+        CREATE TABLE IF NOT EXISTS results(
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        scan_id INTEGER,
+        host TEXT,
+        port INTEGER,
+        state TEXT,
+
+        FOREIGN KEY (scan_id) REFERENCES  scans(id)
+        );""")
+    con.commit()
+    con.close()
+
+def create_scan(timestamp, scope):
+    con = get_connection()
+    cur = con.cursor()
+    cur.execute("""INSERT INTO scans (started_at, scope) VALUES(?, ?)""", (timestamp, scope))
+
+    scan_id = cur.lastrowid
+    con.commit()
+    con.close()
+
+    return scan_id
+
+def insert_result(scan_id, host, port, state):
+    con = get_connection()
+    cur = con.cursor()
+    
+    cur.execute("""INSERT INTO results (scan_id, host, port, state) VALUES (?,?,?,?)""", (scan_id, host, port, state))
+    con.commit()
+    con.close()
+
+def get_last_scan():
+    pass
+
+def get_previous_scan():
+    pass
